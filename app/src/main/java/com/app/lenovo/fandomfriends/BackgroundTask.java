3cf56af777a459f9bccfc15bddc751879d8654a8
PalAditya <ap37@iitbbs.ac.in>
2018-02-04 21:49:50 +0530

    --Change from nearby to SQL

---
+++ app/src/main/java/com/app/lenovo/fandomfriends/BackgroundTask.java
@@ -0,0 +1,130 @@
+package com.app.lenovo.fandomfriends;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class BackgroundTask extends AsyncTask<String, Void, String> {
+    AlertDialog alertDialog;
+    Context ctx;
+
+    BackgroundTask(Context ctx){
+
+        this.ctx = ctx;
+    }
+    @Override
+    protected void onPreExecute() {
+
+        alertDialog = new AlertDialog.Builder(ctx).create();
+        alertDialog.setTitle("Finding information");
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String reg_url = "http://203.124.114.1/Kryptex5.0/signup.php";
+        String login_url = "http://192.168.0.10/webapp/login.php";
+        String method = params [0];
+        if (method.equals("register") ){
+            String name = params [1];
+            String lati = params [2];
+            String longi = params [3];
+            String f1 = params [4];
+            String f2 = params [5];
+            try {
+                URL url = new URL(reg_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                OutputStream OS = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(OS, "UTF-8"));
+
+                String data = URLEncoder.encode("name", "UTF-8") + "=" + URLEncoder.encode(name, "UTF-8") + "&" +
+                        URLEncoder.encode("lati", "UTF-8") + "=" + URLEncoder.encode(lati, "UTF-8") + "&" +
+                        URLEncoder.encode("longi", "UTF-8") + "=" + URLEncoder.encode(longi, "UTF-8")+ "&" +
+                        URLEncoder.encode("f1", "UTF-8") + "=" + URLEncoder.encode(f1, "UTF-8")+ "&" +
+                        URLEncoder.encode("f2", "UTF-8") + "=" + URLEncoder.encode(f2, "UTF-8");
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                OS.close();
+                InputStream IS = httpURLConnection.getInputStream();
+                IS.close();
+                httpURLConnection.disconnect();
+                return "Registration Has Been Successful.";
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else if(method.equals("login"))
+        {
+            String login_name = params[1];
+            String login_pass = params[2];
+            try {
+                URL url = new URL(login_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+                String data = URLEncoder.encode("login_name","UTF-8")+"="+URLEncoder.encode(login_name,"UTF-8")+"&"+
+                        URLEncoder.encode("login_pass","UTF-8")+"="+URLEncoder.encode(login_pass,"UTF-8");
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader (new InputStreamReader(inputStream,"iso-8859-1"));
+                String response = "";
+                String line = "";
+                while ((line = bufferedReader.readLine())!=null)
+                {
+                    response+= line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return response;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        if(result.equals("Registration Has Been Successful."))
+        {
+            Toast.makeText(ctx, result, Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            alertDialog.setMessage(result);
+            alertDialog.show();
+        }
+
+    }
+}
--- app/src/main/java/com/app/lenovo/fandomfriends/MainActivity.java
+++ app/src/main/java/com/app/lenovo/fandomfriends/MainActivity.java
@@ -1,8 +1,13 @@
 package com.app.lenovo.fandomfriends;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,8 +16,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.nearby.Nearby;
 import com.google.android.gms.nearby.connection.AdvertisingOptions;
 import com.google.android.gms.nearby.connection.ConnectionInfo;
@@ -29,37 +40,61 @@ import com.google.android.gms.nearby.connection.Strategy;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
     private static final Strategy STRATEGY = Strategy.P2P_CLUSTER;
     private ConnectionsClient connectionsClient;
     private String opponentName;
-    private String profile="Aditya";
+    private String profile = "Aditya";
     SharedPreferences sharedPreferences;
     String biotext = "bio";
     String bioedittext;
-
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private final static   int ACCESS_FINE_LOCATION = 0;
+    private  final static int ACCESS_COARSE_LOCATION = 1;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private double currentLatitude;
+    int flag=0;
+    private double currentLongitude;
+    String name,lati,longi,fandom1,fandom2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                // The next two lines tell the new client that “this” current class will handle connection stuff
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                //fourth line adds the LocationServices API endpoint from GooglePlayServices
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
+
         sharedPreferences = getSharedPreferences(biotext, Context.MODE_PRIVATE);
         EditText bio = (EditText) findViewById(R.id.Bio);
         bioedittext = sharedPreferences.getString("bio", "Enter your bio here");
         bio.setText(bioedittext);
-        final ImageButton edit=(ImageButton)findViewById(R.id.imageButton);
+        final ImageButton edit = (ImageButton) findViewById(R.id.imageButton);
         edit.setOnClickListener(new View.OnClickListener() {
 
             EditText bio = (EditText) findViewById(R.id.Bio);
+
             //int flag = 1;
             @Override
-            public void onClick(View view){
-                if(!bio.isEnabled()) {
+            public void onClick(View view) {
+                if (!bio.isEnabled()) {
                     bio.setEnabled(true);
                     bio.setCursorVisible(true);
                     bio.setFocusableInTouchMode(true);
                     edit.setImageResource(R.drawable.nrg);
-                }
-                else{
+                } else {
                     String bioedit = bio.getText().toString();
                     SharedPreferences.Editor editor = sharedPreferences.edit();
                     editor.putString(biotext, bioedit);
@@ -76,6 +111,162 @@ public class MainActivity extends AppCompatActivity {
         startAdvertising();
         startDiscovery();
     }
+    public void btRegister(View view)
+    {
+        /*first_name =  ET_FIRST_NAME.getText().toString();
+        last_name = ET_LAST_NAME.getText().toString();
+        address = ET_ADDRESS.getText().toString();
+        email = ET_EMAIL.getText().toString();
+        password = ET_PASSWORD.getText().toString();*/
+        name="HighlyUniqueName";
+        fandom1="HarryPotter";
+        fandom2="Ben10";
+        String method = "register";
+        BackgroundTask backgroundTask = new BackgroundTask(this);
+        backgroundTask.execute(method, name,lati,longi,fandom1,fandom2);
+        finish();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //Now lets connect to the API
+        mGoogleApiClient.connect();
+        //Toast.makeText(this, "In onResume()",  Toast.LENGTH_LONG).show();
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.v(this.getClass().getSimpleName(), "onPause()");
+
+        //Disconnect from API onPause()
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        //Toast.makeText(this, "Tried to connect", Toast.LENGTH_LONG).show();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                Toast.makeText(this, "Requesting...", Toast.LENGTH_LONG).show();
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        ACCESS_FINE_LOCATION);
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        ACCESS_COARSE_LOCATION);
+
+                //return;
+            }
+            /*if(flag==0) {
+                Toast.makeText(this, "Flag is 0...aborting", Toast.LENGTH_LONG).show();
+                return;
+            }*/
+            Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        //Toast.makeText(this, "Connected", Toast.LENGTH_LONG).show();
+            if (location == null) {
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+            } else {
+                //If everything went fine lets get latitude and longitude
+                currentLatitude = location.getLatitude();
+                currentLongitude = location.getLongitude();
+                lati=currentLatitude+"";
+                longi=currentLongitude+"";
+                //Toast.makeText(this, currentLatitude + " WORKS " + currentLongitude + "", Toast.LENGTH_LONG).show();
+                TextView tv=findViewById(R.id.FandomsFollowed);
+                tv.setText(currentLatitude+" , "+currentLongitude);
+            }
+        }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case ACCESS_COARSE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    flag++;
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                //return;
+            }
+
+            case ACCESS_FINE_LOCATION:
+            {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    flag++;
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                //return;
+            }
+        }
+    }
+    @Override
+    public void onConnectionSuspended(int i) {}
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+            /*
+             * Google Play services can resolve some errors it detects.
+             * If the error has a resolution, try sending an Intent to
+             * start a Google Play services activity that can resolve
+             * error.
+             */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                    /*
+                     * Thrown if Google Play services canceled the original
+                     * PendingIntent
+                     */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+                /*
+                 * If no resolution is available, display a dialog to the
+                 * user with the error.
+                 */
+            Log.e("Error", "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+        currentLatitude = location.getLatitude();
+        currentLongitude = location.getLongitude();
+
+        Toast.makeText(this, currentLatitude + " WORKS " + currentLongitude + "", Toast.LENGTH_LONG).show();
+    }
+
+
+
     private final EndpointDiscoveryCallback endpointDiscoveryCallback =
             new EndpointDiscoveryCallback() {
                 @Override
