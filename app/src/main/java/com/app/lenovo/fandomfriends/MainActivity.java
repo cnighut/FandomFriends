7207242950bb840adaa49a71cb2bc59c036701b5
qawbecrdteyf <qawbecrdteyf@gmail.com>
2018-03-06 00:10:01 +0530

    ImgUpload kinda!

--- app/src/main/java/com/app/lenovo/fandomfriends/MainActivity.java
+++ app/src/main/java/com/app/lenovo/fandomfriends/MainActivity.java
@@ -17,6 +17,7 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -35,6 +36,7 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
@@ -182,15 +184,25 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
                 CircleImageView img = findViewById(R.id.Profilepic);
                 img.setImageBitmap(bitmap);
+                String img_string = getStringImage(bitmap);
+                UploadImage uploadImage = new UploadImage(this);
+                uploadImage.execute(img_string, "ChiragNighut");
 
             } catch (FileNotFoundException e) {
-                // TODO Auto-generated catch block
+
                 e.printStackTrace();
             } catch (IOException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         }
+    }
+
+    private String getStringImage(Bitmap bmp) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
     }
 
     public void StartIt(View view)
---
+++ app/src/main/java/com/app/lenovo/fandomfriends/UploadImage.java
@@ -0,0 +1,86 @@
+package com.app.lenovo.fandomfriends;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+/**
+ * Created by qawbecrdteyf on 3/5/18.
+ */
+
+class UploadImage extends AsyncTask<String,Void,String> {
+
+    Context ctx;
+
+    UploadImage(Context ctx){
+
+        this.ctx = ctx;
+    }
+
+    //ProgressDialog loading;
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        //loading = ProgressDialog.show(this,"Please wait...","uploading",false,false);
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        //loading.dismiss();
+        Toast.makeText(ctx,s,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        /*RequestHandler rh = new RequestHandler();
+        HashMap<String,String> param = new HashMap<String,String>();
+        param.put(KEY_TEXT,house);
+        param.put(KEY_TEXT1,build);
+        param.put(KEY_IMAGE,image);
+        param.put(KEY_IMAGE1,image1);
+        String result = rh.sendPostRequest(UPLOAD_URL, param);*/
+
+
+        try {
+                URL url;
+                String reg_url = "http://almat.almafiesta.com/Kryptex5.0/ImgUpload.php";
+                url = new URL(reg_url);
+            HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                OutputStream OS = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(OS, "UTF-8"));
+                String imgstring = params[0];
+                String name=params[1];
+            String data = URLEncoder.encode("name", "UTF-8") + "=" + URLEncoder.encode(name, "UTF-8") + "&" +
+                    URLEncoder.encode("imgstring", "UTF-8") + "=" + URLEncoder.encode(imgstring, "UTF-8");
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                OS.close();
+                InputStream IS = httpURLConnection.getInputStream();
+                IS.close();
+                httpURLConnection.disconnect();
+                return "ImgUpload done!";
+            } catch (MalformedURLException e) {
+            Log.d("error", "error is good");
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        return null;
+    }
+
+}
